5d1e0915016bdcda467e06a66e6621c9abe906ae
nicvroyz <nicolavoyarzo@gmail.com>
2023-11-24 04:17:04 -0400

    FIx

--- assets/js/validations.js
+++ assets/js/validations.js
@@ -7,194 +7,217 @@ if (typeof jQuery == 'undefined') {
 // Funciones de validación en tiempo real
 function asignarValidaciones() {
     // Event listener para el campo de "Nombre y Apellido"
-    $('#nombre_apellido').on('input', validarNombreApellido);
+    $('#nombre_apellido').on('input', function () {
+        validarNombreApellido([]);
+    });
 
     // Event listener para el campo de "RUT"
     $('#rut').on('input', function () {
-        validarRUT();
+        validarRUT([]);
         validarRUTDuplicado();
     });
 
     // Event listener para el campo de "Email"
-    $('#email').on('input', validarEmail);
+    $('#email').on('input', function () {
+        validarEmail([]);
+    });
 
     // Event listener para el campo de "Alias"
-    $('#alias').on('input', validarAlias);
+    $('#alias').on('input', function () {
+        validarAlias([]);
+    });
 
     // Event listener para el combo de "Región", "Provincia" y "Comuna"
-    $('#region, #provincia, #comuna').on('change', validarRegionProvinciaComuna);
+    $('#region, #provincia, #comuna').on('change', function () {
+        validarRegionProvinciaComuna([]);
+    });
 
     // Event listener para el combo de "Candidato"
-    $('#candidato').on('change', validarCandidato);
+    $('#candidato').on('change', function () {
+        validarCandidato([]);
+    });
 
     // Event listener para los checkboxes de "Como se enteró de Nosotros"
-    $('input[name="como_se_entero[]"]').on('change', validarComoSeEntero);
+    $('input[name="como_se_entero[]"]').on('change', function () {
+        validarComoSeEntero([]);
+    });
 
     // Añadir más funciones de validación en tiempo real según tus necesidades
 }
 
+// Función para validar el campo de "Nombre y Apellido"
+function validarNombreApellido(errores) {
+    var nombreApellidoInput = 'nombre_apellido';
+    var nombreApellidoValue = $('#' + nombreApellidoInput).val();
 
-    // Función para validar el campo de "Nombre y Apellido"
-    function validarNombreApellido() {
-        var nombreApellidoInput = 'nombre_apellido';
-        var nombreApellidoValue = $('#' + nombreApellidoInput).val();
-
-        if (!nombreApellidoValue.trim()) {
-            mostrarError(nombreApellidoInput, 'El Nombre y Apellido no debe quedar en blanco.');
-        } else {
-            quitarError(nombreApellidoInput);
-        }
+    if (!nombreApellidoValue.trim()) {
+        errores.push('El Nombre y Apellido no debe quedar en blanco.');
+        mostrarError('error-container', 'El Nombre y Apellido no debe quedar en blanco.');
+    } else {
+        quitarError('error-container');
     }
+}
 
-    // Función para validar el campo de "Alias"
-    function validarAlias() {
-        var aliasInput = 'alias';
-        var aliasValue = $('#' + aliasInput).val();
+// Función para validar el campo de "Alias"
+function validarAlias(errores) {
+    var aliasInput = 'alias';
+    var aliasValue = $('#' + aliasInput).val();
 
-        if (aliasValue.length < 6 || !/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+$/.test(aliasValue)) {
-            mostrarError(aliasInput, 'El Alias debe tener al menos 6 caracteres y contener letras y números.');
-        } else {
-            quitarError(aliasInput);
-        }
+    if (aliasValue.length < 6 || !/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+$/.test(aliasValue)) {
+        errores.push('El Alias debe tener al menos 6 caracteres y contener letras y números.');
+        mostrarError('error-container', 'El Alias debe tener al menos 6 caracteres y contener letras y números.');
+    } else {
+        quitarError('error-container');
     }
+}
 
-    // Función para validar el campo de "RUT"
-    function validarRUT() {
+// Función para validar el RUT duplicado
+function validarRUTDuplicado() {
+    var rutInput = 'rut';
+    var rutValue = $('#' + rutInput).val();
+
+    $.ajax({
+        type: 'POST',
+        url: 'procesar_voto.php',
+        data: { action: 'validar_rut_duplicado', rut: rutValue },
+        success: function (response) {
+            if (response === 'true') {
+                quitarError('error-container'); // El RUT no está duplicado
+            } else {
+                mostrarError('error-container', 'Ya se ha registrado un voto con este RUT.');
+            }
+        },
+        error: function (error) {
+            console.log(error);
+            mostrarError('error-container', 'Error en la validación del RUT duplicado.');
+        }
+    });
+}
+
+
+// Función para validar el RUT
+function validarRUT() {
+    return new Promise(function (resolve, reject) {
         var rutInput = 'rut';
         var rutValue = $('#' + rutInput).val();
 
-        // Realizar una solicitud AJAX
         $.ajax({
             type: 'POST',
-            url: 'validar_rut.php', 
+            url: 'validar_rut.php',
             data: { rut: rutValue },
             success: function (response) {
+                var errores = []; // Array para almacenar errores
+
                 if (response === 'true') {
                     // Validar duplicado de RUT después de verificar su formato
-                    validarRUTDuplicado(rutInput, rutValue);
+                    validarRUTDuplicado()
+                        .then(function () {
+                            quitarError('error-container'); // Limpiar errores anteriores
+                            resolve(true); // Ambas validaciones pasaron
+                        })
+                        .catch(function (error) {
+                            errores.push(error); // Agregar error al array
+                            mostrarErrores('error-container', errores);
+                            reject(false); // Error en la validación de duplicado
+                        });
                 } else {
-                    mostrarError(rutInput, 'El RUT no es válido.');
+                    errores.push('El RUT no es válido.'); // Agregar error al array
+                    mostrarErrores('error-container', errores);
+                    reject(false); // El RUT no es válido
                 }
             },
             error: function (error) {
                 console.log(error);
+                errores.push('Error en la validación del RUT.'); // Agregar error al array
+                mostrarErrores('error-container', errores);
+                reject(false); // Error en la validación del RUT
             }
         });
+    });
+}
+
+// Función para validar el campo de "Email"
+function validarEmail(errores) {
+    var emailInput = 'email';
+    var emailValue = $('#' + emailInput).val();
+
+    var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!regexEmail.test(emailValue)) {
+        errores.push('El formato del correo electrónico no es válido.');
+        mostrarError('error-container', 'El formato del correo electrónico no es válido.');
+    } else {
+        quitarError('error-container');
     }
+}
 
-    // Función para validar el campo de "Email"
-    function validarEmail() {
-        var emailInput = 'email';
-        var emailValue = $('#' + emailInput).val();
+// Función para validar los combos de "Región", "Provincia" y "Comuna"
+function validarRegionProvinciaComuna(errores) {
+    var regionSelect = 'region';
+    var provinciaSelect = 'provincia';
+    var comunaSelect = 'comuna';
 
-        // Utiliza una expresión regular para validar el formato del correo electrónico
-        var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var regionValue = $('#' + regionSelect).val();
+    var provinciaValue = $('#' + provinciaSelect).val();
+    var comunaValue = $('#' + comunaSelect).val();
 
-        if (!regexEmail.test(emailValue)) {
-            mostrarError(emailInput, 'El formato del correo electrónico no es válido.');
-        } else {
-            quitarError(emailInput);
-        }
+    if (regionValue === '' || provinciaValue === '' || comunaValue === '') {
+        errores.push('Los campos Región, Provincia y Comuna no deben quedar en blanco.');
+        mostrarError('error-container', 'Los campos Región, Provincia y Comuna no deben quedar en blanco.');
+    } else {
+        quitarError('error-container');
     }
+}
 
-    // Función para validar los combos de "Región", "Provincia" y "Comuna"
-    function validarRegionProvinciaComuna() {
-        var regionSelect = 'region';
-        var provinciaSelect = 'provincia';
-        var comunaSelect = 'comuna';
+// Función para validar el combo de "Candidato"
+function validarCandidato(errores) {
+    var candidatoSelect = 'candidato';
 
-        var regionValue = $('#' + regionSelect).val();
-        var provinciaValue = $('#' + provinciaSelect).val();
-        var comunaValue = $('#' + comunaSelect).val();
-
-        if (regionValue === '' || provinciaValue === '' || comunaValue === '') {
-            mostrarError('error-container', 'Los campos Región, Provincia y Comuna no deben quedar en blanco.');
-            return false;
-        } else {
-            quitarError('error-container');
-            return true;
-        }
+    if ($('#' + candidatoSelect).val() === '') {
+        errores.push('El campo Candidato no debe quedar en blanco.');
+        mostrarError('error-container', 'El campo Candidato no debe quedar en blanco.');
+    } else {
+        quitarError('error-container');
     }
+}
 
-    // Función para validar el combo de "Candidato"
-    function validarCandidato() {
-        var candidatoSelect = 'candidato';
+// Función para validar los checkboxes de "Como se enteró de Nosotros"
+function validarComoSeEntero(errores) {
+    var checkboxesComoSeEntero = $('input[name="como_se_entero[]"]');
+    var checkboxesMarcados = checkboxesComoSeEntero.filter(':checked').length;
 
-        if ($('#' + candidatoSelect).val() === '') {
-            mostrarError('error-container', 'El campo Candidato no debe quedar en blanco.');
-        } else {
-            quitarError('error-container');
-        }
+    if (checkboxesMarcados < 2) {
+        errores.push('Debe elegir al menos dos opciones en "Como se enteró de Nosotros".');
+        mostrarErrores('error-container', errores);
+    } else {
+        quitarError('error-container');
     }
+}
 
-    // Función para validar los checkboxes de "Como se enteró de Nosotros"
-    function validarComoSeEntero() {
-        var checkboxesComoSeEntero = $('input[name="como_se_entero[]"]');
-        
-        // Contar cuántos checkboxes están marcados
-        var checkboxesMarcados = checkboxesComoSeEntero.filter(':checked').length;
+// Función para mostrar un mensaje de error
+function mostrarError(contenedor, mensaje) {
+    var errorContainer = $('#' + contenedor);
+    errorContainer.empty();
+    errorContainer.append('<p class="alert alert-danger">' + mensaje + '</p>');
+    console.log(mensaje);
+}
 
-        if (checkboxesMarcados < 2) {
-            // Mostrar error
-            mostrarError('error-container', 'Debe elegir al menos dos opciones en "Como se enteró de Nosotros".');
-        } else {
-            // No hay error, quitar mensaje de error
-            quitarError('error-container');
-        }
+// Función para mostrar múltiples errores en el contenedor
+function mostrarErrores(contenedor, errores) {
+    var errorContainer = $('#' + contenedor);
+    errorContainer.empty();
+    for (var i = 0; i < errores.length; i++) {
+        errorContainer.append('<p class="alert alert-danger">' + errores[i] + '</p>');
     }
+    console.log(errores);
+}
 
-    // Función para validar el RUT duplicado
-    function validarRUTDuplicado(rutInput, rutValue) {
-        $.ajax({
-            type: 'POST',
-            url: 'procesar_voto.php', 
-            data: { action: 'validar_rut_duplicado', rut: rutValue },
-            success: function (response) {
-                if (response === 'true') {
-                    quitarError(rutInput); // El RUT no está duplicado
-                } else {
-                    mostrarError(rutInput, 'Ya se ha registrado un voto con este RUT.');
-                }
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        });
-    }
+// Función para quitar el mensaje de error
+function quitarError(contenedor) {
+    $('#' + contenedor).empty();
+}
 
-    // Función para mostrar un mensaje de error
-    function mostrarError(contenedor, mensaje) {
-        // Muestra los mensajes de error en el contenedor de errores
-        var errorContainer = $('#' + contenedor);
-    
-        // Borra mensajes anteriores
-        errorContainer.empty();
-    
-        // Agrega el nuevo mensaje de error
-        errorContainer.append('<p class="alert alert-danger">' + mensaje + '</p>');
-    
-        // Loguea el mensaje en la consola para propósitos de depuración
-        console.log(mensaje);
-    }
-
-    // Función para quitar el mensaje de error
-    function quitarError(contenedor) {
-        // Limpia el contenido del contenedor de errores
-        $('#' + contenedor).empty();
-    }
-
-    // Validación final del formulario antes de enviarlo al servidor
-    function validarFormulario() {
-        var esValido = true;
-    
-        esValido = esValido && validarNombreApellido();
-        esValido = esValido && validarRUT();
-        esValido = esValido && !validarRUTDuplicado();
-        esValido = esValido && validarEmail();
-        esValido = esValido && validarAlias();
-        esValido = esValido && validarRegionProvinciaComuna();
-        esValido = esValido && validarCandidato();
-        esValido = esValido && validarComoSeEntero();
-    
-        return esValido;
-    };
+// Función para quitar todos los mensajes de error del contenedor
+function quitarErrores(contenedor) {
+    $('#' + contenedor).empty();
+}
